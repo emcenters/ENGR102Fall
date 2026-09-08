609a7b1e2f3d487cf62299abd90adbbbb9a955fb
emcenters <emmanuellechern3@gmail.com>
2026-09-08 14:51:28 -0500

    In class example of conditionals.

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print("Hello World")
+# Create one line of a conditional statement that: sets flagged equals to if a oz ≥ 9, 
+# the difference of past and current oz ≥ 5 and is positive, and false otherwise
+oz = [0 for _ in range(4)]
+i = 0
+flagged = (oz[i] >= 9 or (i >=1 and (abs(oz[i-1] - oz[i]) >= 5 and oz[i] > 0)))
+print(flagged)
